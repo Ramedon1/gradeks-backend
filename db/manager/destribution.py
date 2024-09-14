f6b3581295479a1ed2c43f5456682c2b4fe387d2
Ramedon <you@example.com>
2024-09-14 21:23:44 +0500

    ✨ Add edit distribution

--- db/manager/destribution.py
+++ db/manager/destribution.py
@@ -10,7 +10,7 @@ from db.models.distribution import Distribution
 
 class DbManagerUsers(DbManagerBase):
     async def get_distribution(
-        self, user_id: str | UUID, outer_session: AsyncSession | None = None
+            self, user_id: str | UUID, outer_session: AsyncSession | None = None
     ) -> Distribution | None:
         async with self.session_manager(outer_session) as session:
             statement = select(Distribution.distribution_status).where(
@@ -22,10 +22,10 @@ class DbManagerUsers(DbManagerBase):
             return distribution
 
     async def get_distribution_users(
-        self,
-        user_id: str | UUID,
-        distribution_type: str,
-        outer_session: AsyncSession | None = None,
+            self,
+            user_id: str | UUID,
+            distribution_type: str,
+            outer_session: AsyncSession | None = None,
     ) -> list[Distribution]:
         async with self.session_manager(outer_session) as session:
             statement = (
@@ -36,5 +36,18 @@ class DbManagerUsers(DbManagerBase):
             )
             result = await session.exec(statement)
             distribution = result.all()
+
+            return distribution
+
+    async def edit_distribution(self, user_id: str | UUID, distribution_type: str,
+                                outer_session: AsyncSession | None = None) -> Distribution:
+        async with self.session_manager(outer_session) as session:
+            statement = select(Distribution).where(Distribution.user_id == user_id).where(
+                Distribution.distribution_type == distribution_type)
+            result = await session.exec(statement)
+            distribution = result.one_or_none()
+            distribution.distribution_status = True
+            await session.commit()
+            await session.refresh(distribution)
 
             return distribution
